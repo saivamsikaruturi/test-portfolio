256d83450f939dd1e60f9f95edbcde31a29ac7d5
VAMSI KARUTURI <karuturi-sai-naga.vamsi-krishna@siemens.com>
2023-12-29 10:38:59 +0530

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> ABHIBHAB MITRA</strong>
+                <strong className="main-name"> VAMSI KARUTURI</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
